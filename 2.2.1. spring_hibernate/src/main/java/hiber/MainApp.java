2b8b99e3fa66c201b45f1db0164e1729b3804136
kekeke <muscarin69@gmail.com>
2025-02-24 05:12:53 +0300

    Solution

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,40 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        User max = new User("Max", "Max", "example@mail.com");
+        User viktoria = new User("Vika", "Vika", "example1@mail.com");
+        User kir = new User("Kir", "Kir", "example2@mail.com");
+        User anna = new User("Anya", "Anya", "example3@mail.com");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        Car ford = new Car("Ford", 250);
+        Car opel = new Car("Opel", 48);
+        Car honda = new Car("Honda", 3);
+        Car volvo = new Car("Volvo", 300);
 
-      context.close();
-   }
+        userService.add(max.setCar(ford).setUser(max));
+        userService.add(viktoria.setCar(opel).setUser(viktoria));
+        userService.add(kir.setCar(honda).setUser(kir));
+        userService.add(anna.setCar(volvo).setUser(anna));
+
+        for (User user : userService.listUsers()) {
+            System.out.println(user + " " + user.getCar());
+        }
+
+        System.out.println(userService.getUserByCar("Honda", 3));
+
+        context.close();
+    }
 }
